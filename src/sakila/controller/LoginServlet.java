d5b0cfd7ab4c18ed1a9003280d10c66f7a65f25d
GD768 <GD768@DESKTOP-T3HBREL>
2020-10-19 16:36:53 +0900

    s

--- src/sakila/controller/LoginServlet.java
+++ src/sakila/controller/LoginServlet.java
@@ -1,28 +1,39 @@
 package sakila.controller;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import gd.board.service.BoardService;
+import gd.board.vo.Board;
 
-import com.oreilly.servlet.CacheHttpServlet;
-
-
-@WebService("/LoginServlet")
-public class LoginServlet extends CacheHttpServlet {
-	private static final long serialVersionUID = 1L;
-
-  
+@WebServlet("/BoardController")
+public class BoardController extends HttpServlet {
+	private BoardService boardService;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSesson session = request.getSession();
-		if(session.getAttribute("loginStaff") == null) {
-			response.sendRedirect(request.getContextPath()+"/auth/IndexServlet");
-			return;
+		int currentPage = 1;
+		if(request.getParameter("currentPage") != null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
 		}
-		request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request. response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		int rowPerPage = 10;
+		if(request.getParameter("rowPerPage") != null) {
+			rowPerPage = Integer.parseInt(request.getParameter("rowPerPage"));
+		}
+		
+		boardService = new BoardService();
+		Map<String, Object> map = boardService.getBoardListByPage(currentPage, rowPerPage);
+		List<Board> list = (List<Board>)(map.get("list"));
+		int boardTotalCount = (Integer)(map.get("boardTotalCount"));
+		request.setAttribute("list", list);
+		request.setAttribute("boardTotalCount", boardTotalCount);
+		// request.setAttribute("lastPage", lastPage);
+		request.getRequestDispatcher("/WEB-INF/views/boardList.jsp");
 	}
 
 }
